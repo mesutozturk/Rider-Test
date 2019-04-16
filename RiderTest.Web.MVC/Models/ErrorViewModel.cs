a9926e65451ce9c3574a4c8a5377f21471e6388b
Mesut Öztürk <mesutoztrk@msn.com>
2019-04-16 22:20:54 +0300

    init rider

---
+++ RiderTest.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RiderTest.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
